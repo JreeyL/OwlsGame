a130facfccb35484f9901ed2e0404f7666ddd259
Baron.Lee <baronlee92@gmail.com>
2025-03-28 00:04:28 +0000

    Update CustomErrorController for figure out 1 Security Hotspots. ErrorController only accepts GetMapping.

--- src/main/java/org/OwlsGame/controllers/CustomErrorController.java
+++ src/main/java/org/OwlsGame/controllers/CustomErrorController.java
@@ -3,6 +3,7 @@ package org.OwlsGame.controllers;
 import jakarta.servlet.RequestDispatcher;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
